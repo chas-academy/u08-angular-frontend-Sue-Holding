2786d1b667ba5a715b294f04b1faa1ffd6fa774c
Sue-Holding <sue.holding55@gmail.com>
2025-04-28 20:25:38 +0200

    updated routes, added provideHttpClient to config.ts and fetch method to animal-list

--- amazing-animals-app/src/app/animals/animal-list/animal-list.component.ts
+++ amazing-animals-app/src/app/animals/animal-list/animal-list.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AnimalService } from '../animal-service';
+import { Animal } from '../../shared/models/animal-model';
 
 @Component({
   selector: 'app-animal-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './animal-list.component.html',
   styleUrl: './animal-list.component.css'
 })
-export class AnimalListComponent {
+export class AnimalListComponent implements OnInit {
+  animals: Animal[] = [];
+  isLoading = true;
+  error: string | null = null;
 
+  constructor(private animalService: AnimalService) {}
+
+  ngOnInit(): void {
+    this.getAnimals();
+  }
+
+  getAnimals(): void {
+    this.animalService.getAnimals().subscribe({
+      next: (data) => {
+        this.animals = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Error fetching animals:', err);
+        this.error = 'Failed to load animals';
+        this.isLoading = false;
+      }
+    });
+  }
 }
--- amazing-animals-app/src/app/app.routes.ts
+++ amazing-animals-app/src/app/app.routes.ts
@@ -5,9 +5,34 @@ import { AnimalFormComponent } from './animals/animal-form/animal-form.component
 
 export const routes: Routes = [
     { path: '',redirectTo: 'animals', pathMatch: 'full' },
-    { path: 'animals', component: AnimalListComponent },
-    { path: 'animals/:id', component: AnimalDetailComponent },
-    { path: 'animals/:id/edit', component: AnimalFormComponent },
-    { path: 'create', component: AnimalFormComponent },
+    { path: 'animals', 
+        loadComponent: () => import('./animals/animal-list/animal-list.component')
+        .then(m => m.AnimalListComponent)
+    },
+    { path: 'animals/:id', 
+        loadComponent: () => import('./animals/animal-detail/animal-detail.component')
+        .then(m => m.AnimalDetailComponent)
+    },
+    { path: 'animals/:id/edit', 
+        loadComponent: () => import('./animals/animal-form/animal-form.component')
+        .then(m => m.AnimalFormComponent)
+    },
+    { path: 'create', 
+        loadComponent: () => import('./animals/animal-form/animal-form.component')
+        .then(m => m.AnimalFormComponent)
+    },
     { path: '**', redirectTo: 'animals' },
 ];
+
+// export const routes: Routes = [
+//     { path: '',redirectTo: 'animals', pathMatch: 'full' },
+//     { path: 'animals', 
+//         component: AnimalListComponent },
+//     { path: 'animals/:id', 
+//         component: AnimalDetailComponent },
+//     { path: 'animals/:id/edit', 
+//         component: AnimalFormComponent },
+//     { path: 'create', 
+//         component: AnimalFormComponent },
+//     { path: '**', redirectTo: 'animals' },
+// ];
