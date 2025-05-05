562ddda523985a15c4ce10bacf577890d9630fcf
Sue-Holding <sue.holding55@gmail.com>
2025-05-05 11:16:31 +0200

    added search by animal name form to list components

--- amazing-animals-app/src/app/animals/animal-list/animal-list.component.ts
+++ amazing-animals-app/src/app/animals/animal-list/animal-list.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { AnimalService } from '../animal-service';
 import { Animal } from '../../shared/models/animal-model';
 
 @Component({
   selector: 'app-animal-list',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './animal-list.component.html',
   styleUrl: './animal-list.component.css'
 })
 
 export class AnimalListComponent implements OnInit {
   animals: Animal[] = [];
+  filteredAnimals: Animal[] = [];
+  searchName: string = '';
   isLoading = true;
   error: string | null = null;
-
+  
   constructor(private animalService: AnimalService) {}
 
   ngOnInit(): void {
@@ -28,6 +31,7 @@ export class AnimalListComponent implements OnInit {
       next: (data) => {
         console.log('Animal list loaded:', this.animals);
         this.animals = data;
+        this.filteredAnimals = data;
         this.isLoading = false;
       },
       error: (err) => {
@@ -37,6 +41,17 @@ export class AnimalListComponent implements OnInit {
       }
     });
   }
+
+  onSearch() {
+    if (this.searchName.trim() === '') {
+      this.filteredAnimals = this.animals;  // show all if empty
+    } else {
+      this.filteredAnimals = this.animals.filter(animal =>
+        animal.name.toLowerCase().includes(this.searchName.toLowerCase())
+      );
+    }
+  }
+  
 
   clearAnimalList() {
     this.animals = [];
