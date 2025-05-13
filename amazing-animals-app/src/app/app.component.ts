f44615d7dcb06d060c919bdf5dccb69fedd99887
Sue-Holding <sue.holding55@gmail.com>
2025-05-13 12:30:59 +0200

    styling to navbar menu for better view on mobile

--- amazing-animals-app/src/app/app.component.ts
+++ amazing-animals-app/src/app/app.component.ts
@@ -9,4 +9,9 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 })
 export class AppComponent {
   title = 'amazing-animals-app';
+  menuOpen = false;
+
+  logout() {
+    console.log('Logged out'); // logout code to be added later
+  }
 }
